2e42cf9df3a0567f099e1d7107d86b7a71acad4a
Mthor5 <matt@mattthorntondesign.com>
2025-12-13 23:55:56 -0800

    Render Shop page on /shop route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Shop from './Shop.jsx'
+
+const normalizePath = (path) => {
+  if (!path) return '/'
+  const trimmed = path.replace(/\/+$/, '')
+  return trimmed === '' ? '/' : trimmed
+}
+
+const pathname = normalizePath(window.location.pathname)
+const isShopRoute = pathname === '/shop'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <StrictMode>{isShopRoute ? <Shop /> : <App />}</StrictMode>,
 )
